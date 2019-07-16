c36a4f551dfc60e3b41e46c489445b5a660ebf52
iver56 <iver56@hotmail.com>
2019-07-16 14:06:29 +0200

    Add simple, ending-based rhyme finder

--- app/rhyme.py
+++ app/rhyme.py
@@ -1,4 +1,17 @@
+from app.utils import get_words
+
+
 def get_rhymes(word):
-    if word == "klase":
-        return [{"word": "tofase", "score": 100}, {"word": "vekstfase", "score": 100}]
-    return []
+    assert type(word) == str
+    assert len(word) >= 1
+
+    words = get_words()
+
+    rhymes = []
+    ending_size = min(3, len(word))
+    ending = word[-ending_size:]
+    for word in words:
+        if word.endswith(ending):
+            rhymes.append({"word": word, "score": 100})
+
+    return rhymes
--- tests/test_rhymes.py
+++ tests/test_rhymes.py
@@ -5,9 +5,9 @@ from app.rhyme import get_rhymes
 
 class TestRhymes(unittest.TestCase):
     def test_get_rhymes(self):
-        rhymes = get_rhymes("klase")
+        rhymes = get_rhymes("drueklase")
         self.assertGreater(len(rhymes), 1)
         rhyme_words = [rhyme["word"] for rhyme in rhymes]
-        self.assertIn("tofase", rhyme_words)
+        self.assertIn("fjase", rhyme_words)
 
-        self.assertEqual(len(get_rhymes("asdfkuhriu")), 0)
+        self.assertEqual(len(get_rhymes("asdfkuhrkjhsdf")), 0)
