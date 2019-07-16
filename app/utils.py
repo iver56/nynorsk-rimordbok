8ace10560e357e40a63064e3ebf6b55f31b4aaf1
iver56 <iver56@hotmail.com>
2019-07-16 13:35:36 +0200

    Cache get_words

--- app/utils.py
+++ app/utils.py
@@ -1,9 +1,11 @@
 import os
+from functools import lru_cache
 from zipfile import ZipFile
 
 from app.settings import DATA_DIR
 
 
+@lru_cache()
 def get_words():
     if not os.path.isfile(DATA_DIR / "fullform_nn.txt"):
         zip_ref = ZipFile(DATA_DIR / "ordbank_nn.zip", "r")
@@ -12,7 +14,7 @@ def get_words():
 
     words = set()
 
-    with open(DATA_DIR / "fullform_nn.txt", "r", encoding='cp1252') as words_file:
+    with open(DATA_DIR / "fullform_nn.txt", "r", encoding="cp1252") as words_file:
         for line in words_file.readlines():
             if line.startswith("*") or len(line) < 3:
                 continue
