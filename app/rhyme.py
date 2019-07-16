dc841914c923b7576aa5011fa6567338ef48941d
runeeikemo <rune.eikem@gmail.com>
2019-07-16 19:00:07 +0200

    Make search case-insensitive

--- app/rhyme.py
+++ app/rhyme.py
@@ -5,7 +5,7 @@ from app.utils import get_words
 
 @lru_cache(maxsize=9001)
 def get_rhymes(word):
-    word = word.strip()
+    word = word.strip().lower()
     assert type(word) == str
     assert len(word) >= 1
     words = get_words()
@@ -14,7 +14,7 @@ def get_rhymes(word):
     ending_size = min(3, len(word))
     ending = word[-ending_size:]
     for candidate_word in words:
-        if candidate_word.endswith(ending) and candidate_word != word:
+        if candidate_word.lower().endswith(ending) and candidate_word != word:
             rhymes.append({"word": candidate_word, "score": 100})
 
     return rhymes
