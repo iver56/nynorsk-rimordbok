96972bbc7b22bd6dd7831bc5e2663e56d83821e2
iver56 <iver56@hotmail.com>
2019-07-16 19:37:31 +0200

    Implement some simple ranking

--- tests/test_rhymes.py
+++ tests/test_rhymes.py
@@ -11,12 +11,12 @@ class TestRhymes(unittest.TestCase):
         self.assertIn("fjase", rhyme_words)
 
         self.assertEqual(len(get_rhymes("asdfkuhrkjhsdf")), 0)
-    
+
     def test_search_word_not_included(self):
         rhymes = get_rhymes("drueklase")
         rhyme_words = [rhyme["word"] for rhyme in rhymes]
         self.assertNotIn("drueklase", rhyme_words)
-    
+
     def test_whitespace_in_search_word(self):
         rhymes = get_rhymes(" daggry ")
         rhyme_words = [rhyme["word"] for rhyme in rhymes]
@@ -28,4 +28,14 @@ class TestRhymes(unittest.TestCase):
             if rhyme["word"] == "morgongry":
                 self.assertIn(3, [rhyme["syllables"]])
 
-        
+    def test_ranking_when_rhyme_ends_with_search_word(self):
+        rhymes = get_rhymes("bark")
+        sevjebark_rank = None
+        gjerrigknark_rank = None
+        for i, rhyme in enumerate(rhymes):
+            if rhyme["word"] == "sevjebark":
+                sevjebark_rank = i
+            elif rhyme["word"] == "gjerrigknark":
+                gjerrigknark_rank = i
+
+        self.assertGreater(sevjebark_rank, gjerrigknark_rank)
