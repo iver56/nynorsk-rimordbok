f299e2d9afe4589f6d10e182b949e5f50e64db11
iver56 <iver56@hotmail.com>
2019-07-18 14:07:03 +0200

    Add a function for ranking rhymes with the help of the ranker model

--- tests/test_ranker.py
+++ tests/test_ranker.py
@@ -2,6 +2,8 @@
 
 import unittest
 
+from app.rhyme import get_rhymes
+from ranker.apply_ranker_model import Ranker
 from ranker.train import Vectorizer
 
 
@@ -10,3 +12,13 @@ class TestRankerPrepareData(unittest.TestCase):
         vectorizer = Vectorizer(mode="dummy")
         self.assertEqual(vectorizer.preprocess_word("halloen"), "LLOEN")
         self.assertEqual(vectorizer.preprocess_word("ætt"), "  ÆTT")
+
+
+class TestRanker(unittest.TestCase):
+    def test_rank_rhymes(self):
+        search_word = "rang"
+        rhyme_candidates = ["klokkeslett", "pang", "kransekake"]
+
+        ranker = Ranker()
+        ranked_rhymes = ranker.rank_rhymes(search_word, rhymes=rhyme_candidates)
+        self.assertEqual(ranked_rhymes, ["pang", "kransekake", "klokkeslett"])
